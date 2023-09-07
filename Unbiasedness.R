2c93eede96c145765e41cc99023ef24ba8bd4cfe
Chandraketu Singh <singh.chandraketu@outlook.com>
2023-09-07 09:22:05 +0530

    Update Unbiasedness.R

--- Unbiasedness.R
+++ Unbiasedness.R
@@ -1,1 +1,8 @@
+#Consider the given data set and show that an estimator (sample mean) is an unbiased for population mean.
+## import the data set 
+library(readxl)
+practical2_data <- read_excel("F:/Inference/practical2_data.xlsx")
+View(practical2_data)
+population=practical2_data
+population
 
